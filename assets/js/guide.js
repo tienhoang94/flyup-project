62f02c34a74ec10bcfaa05fcdc3628a07568c5d6
tienhoang94 <tienhvdt03@gmail.com>
2020-12-04 00:15:32 +0700

    cut profile guide

--- assets/js/guide.js
+++ assets/js/guide.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
     if ($(window).width() <= 1023){ 
         $(".fotorama").attr("data-width",$(window).width()-5);
-        console.log($(window).width());
     }
+
+    $(".profile_infor-picture").click(function() {
+        console.log("aaaa");
+        $("#upload-photo").change(function() {
+            var fullPath = document.getElementById('upload-photo').value;
+            if (fullPath) {
+                var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+                var filename = fullPath.substring(startIndex);
+                if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+                    filename = filename.substring(1);
+                }
+                $('.upload-name-file').text(filename);
+            }
+
+           
+        })
+    });
 
     
 });
